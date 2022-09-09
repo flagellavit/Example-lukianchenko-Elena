2ea92630bbff545aa6a9aecd8241d4f9c0c68cb4
flagellavit <4751929zed@gmail.com>
2022-09-10 01:31:18 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Example
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            
+       
+            Console.WriteLine("Введите сумму $:");
+                 string uname = Console.ReadLine();
+            Console.WriteLine("Сколько вы внесли :" + uname+"$");
+                 double name = Convert.ToInt32(uname);
+            Console.WriteLine("Курс сщсталяет 60 руб.");
+            Console.WriteLine("перевод в руб:" + (name=name*60)+ "руб"); 
+
+
+
+            if (name <= 500) 
+            {
+                Console.WriteLine("комиссия составляет 8 руб:" );
+                Console.WriteLine("все что осталось после комиссии :" + (name - 48) + "руб"); //48 это комиссия от 8  
+            }
+            else
+            {
+                Console.WriteLine("комиссия составляет 0,37% руб:");
+                Console.WriteLine("все что осталось после комиссии " + (name - 2.22) + "руб");//2.22 это комиссия от 0.037%  
+            }
+                Console.WriteLine("Спасибо что использовал наш терминал Котик (>^_^)> <(^_^<) ");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        } 
+        
+    }
+}
